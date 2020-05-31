5996a43235aef85377b363a069117b1182b7d2a7
Alex <57685496+AlexSimpler@users.noreply.github.com>
2020-05-31 22:05:14 +0200

    Updated ps1 script

--- misc/setup.ps1
+++ misc/setup.ps1
@@ -1,11 +1,13 @@
 $first = Import-Clixml ./misc/cfg.xml
+$success = "Successfully setup, automatically launching..."
 
 if(!$first) {
         cmd.exe /c "npm install"
         $first = 1
         $first | Export-Clixml ./misc/cfg.xml
+        Write-host $success
         cmd.exe /c "node app"
         
 } 
-Write-Host "Successfully setup, automatically launching..."
+Write-Host $success
 cmd.exe /c "node app"
